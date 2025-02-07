ddf8fbb9dc5a2fb571208aa38daff6767edd9341
Josh Rudge <joshrudge@hotmail.com>
2025-02-07 12:18:00 +0000

    installed everything I need for the backend to work

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,9 @@
-from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Profile
 from .serializers import ProfileSerializer
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
+from rest_framework import generics
 
 class ProfileCreateUpdateView(generics.RetrieveUpdateAPIView):
     queryset = Profile.objects.all()
@@ -14,3 +15,8 @@ class ProfileCreateUpdateView(generics.RetrieveUpdateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    def retrieve(self, request, *args, **kwargs):
+        profile = self.get_object()
+        email = profile.user.email 
+        return Response({"email": email}, status=status.HTTP_200_OK)
